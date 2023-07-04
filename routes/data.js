e24da986051ee5e2ad82540065a3dfad714e2825
ahmadmmi <ahmadmmi1904@gmail.com>
2023-07-04 15:23:22 +0700

    add v2

--- routes/data.js
+++ routes/data.js
@@ -18,4 +18,24 @@ module.exports = async (fastify, options) => {
       } catch (error) {}
     }
   );
+
+  fastify.get(
+    "/v2/data",
+    {
+      schema: {
+        description: "Get Data",
+        tags: ["Data"],
+        summary: "Get Data",
+      },
+    },
+    async (request, reply) => {
+      try {
+        const result = { id: "A0001", attribut: "Atribut A1", value: 1 };
+        reply
+          .code(200)
+          .header("Content-Type", "application/json; charset=utf-8")
+          .send(result);
+      } catch (error) {}
+    }
+  );
 };
